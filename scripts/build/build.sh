3d0e85fa281c0b05bc928af243e398e3b96b1186
Aleksandr Maramzin <maramzinalexandr@gmail.com>
2019-11-29 16:26:14 +0000

    [!] Fixed CMake build harness

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -18,12 +18,26 @@ mkdir ${PROJECT_BUILD_DIR}
 C_FLAGS=""
 
 (
+INSTALL_PREFIX=${2:-../install/}
+#C_FLAGS="-g -Wall -O2"
+C_FLAGS="-Wall"
+LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+#LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
+
 cd ${PROJECT_BUILD_DIR}
 echo "=> CMake is generating playground build system"
-CC=gcc CXX=g++ cmake \
+CC=clang CXX=clang++ cmake \
     -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_FLAGS="${C_FLAGS}" \
+    -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+    -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+    -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+    -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+    -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+    -DHARNESS_USE_LLVM=On \
     "${PROJECT_ROOT_DIR}"
 echo "=> Building playground"
 cmake --build .
