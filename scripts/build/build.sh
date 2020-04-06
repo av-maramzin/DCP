ddaa2adc15d8d92e278cc77ba676100ef54a8a9b
Aleksandr <maramzinalexandr@gmail.com>
2020-04-06 09:59:37 +0100

    [!] Updated main repository with the fixes in olden source base

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -22,7 +22,8 @@ INSTALL_PREFIX=${2:-../install/}
 C_FLAGS="-Wall -g"
 CXX_FLAGS="-Wall -g -fopenmp -std=c++11"
 #C_FLAGS="-Wall"
-LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+#LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
+LINKER_FLAGS="-Wl"
 #LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
 
 BMK_CONFIG_FILE="${OLDEN_HARNESS_DIR}/config/suite_all.txt"
@@ -41,17 +42,18 @@ CC=gcc CXX=g++ cmake \
     -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
     -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
-    -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
-    -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
-    -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
-    -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
     -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
-    -DHARNESS_USE_LLVM=On \
+    -DHARNESS_USE_LLVM=Off \
     -DHARNESS_BMK_CONFIG_FILE=${BMK_CONFIG_FILE} \
     -DBMK_CLASS=${BMK_CLASS} \
     "${PROJECT_ROOT_DIR}"
 echo "=> Building playground"
 cmake --build .
+
+    #-DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+    #-DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+    #-DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+    #-DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
 )
 
 echo "=> Build script finished!"
